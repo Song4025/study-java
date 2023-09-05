b40ec78e8c190511389d6b1f72b4534494838318
Song4025 <thdgus4025@gmail.com>
2023-09-05 17:30:49 +0900

    add lottoMachne method

--- src/com/example/LottoMachineImpl.java
+++ src/com/example/LottoMachineImpl.java
@@ -2,20 +2,34 @@ package com.example;
 
 // 인터페이스를 구현하기 되면 인터페이스 메소드를 오버라이딩 해줘야한다.
 public class LottoMachineImpl implements LottoMachine{
+  private Ball[] balls;
 
   @Override
   public void setBalls(Ball[] balls) {
-
+    this.balls = balls;
   }
 
+  // Math.random란? 결과는 실수로 0.0보단 크고 1.9보단 작은값이 나온다.
   @Override
   public void mix() {
-
+    for(int i=0; i<10000; i++){
+      int x1 = (int)(Math.random() * LottoMachine.MAX_BALL_COUNT);
+      int x2 = (int)(Math.random() * LottoMachine.MAX_BALL_COUNT);
+      if(x1 != x2){
+        Ball tmp = balls[x1]; // 값을 치환할때는 같은 type의 임시변수가 필요하다.
+        balls[x1] = balls[x2];
+        balls[x2] = tmp;
+      }
+    }
   }
 
   @Override
   public Ball[] getBalls() {
-    return new Ball[0];
+    Ball[] result = new Ball[LottoMachine.RETURN_BALL_COUNT];
+    for(int i=0; i<6; i++){
+      result[i] = balls[i];
+    }
+    return result;
   }
 
 }
--- src/com/example/LottoMachineMain.java
+++ src/com/example/LottoMachineMain.java
@@ -9,14 +9,21 @@ public class LottoMachineMain {
   //    .....
   // Ball b45 = new Ball();
   // 이렇게 하면 변수가 45개 필요하니 배열로 만든다. 아래의 뜻은 Balls인스턴스를 45개 만들수 있는 배열이 만들어진다는 뜻.
-  Ball[] balls = new Ball[45];
+  Ball[] balls = new Ball[LottoMachine.MAX_BALL_COUNT];
   for(int i=0; i<45; i++){
     balls[i] = new Ball(i+1);
   }
 
-// 인터페이스는 인스턴트가 될 수 없고 impl은 인터페이스를 구현한 클래스라서 인스턴트가 될 수 있다.
+    // 인터페이스는 인스턴트가 될 수 없고 impl은 인터페이스를 구현한 클래스라서 인스턴트가 될 수 있다.
     // LottoMachine 인스턴트가 생성된다.
     LottoMachine lottoMachine = new LottoMachineImpl();
-  }
+    lottoMachine.setBalls(balls);
+    lottoMachine.mix();
+    lottoMachine.getBalls();
+    Ball[] result = lottoMachine.getBalls();
 
+    for(int i=0; i<result.length; i++){
+      System.out.println(result[i].getNumber());
+    }
+  }
 }
