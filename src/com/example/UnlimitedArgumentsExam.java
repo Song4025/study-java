b43119191abcf4491a7ab738a03d41257a01dbcc
thdgus4025 <thdgus4025@nexlink.co.kr>
2023-09-22 06:51:16 +0900

    update array

--- src/com/example/UnlimitedArgumentsExam.java
+++ src/com/example/UnlimitedArgumentsExam.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UnlimitedArgumentsExam {
     public static void main(String[] args) {
-        System.out.println(sum(5, 10));
+        System.out.println(sum());
         System.out.println(sum(1,2,4,2));
         System.out.println(sum(3,1,2,3,4,2));
+
+        List<Integer> intList = Arrays.asList(new Integer[] {1,2,3,4,5});
+
+        System.out.println(sum(intList.toArray(new Integer[intList.size()])));
     }
 
-    public static int sum(int... args){
+    public static int sum(Integer... args){
         System.out.println("print1 메소드 - args의 길이" + args.length);
         int sum = 0;
         for(int i = 0; i< args.length; i++){
